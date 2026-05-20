d564288e7305805570b0dd67139b40f8d88da78e
ansu-kri <ansukummari@gmail.com>
2026-05-20 12:05:10 +0530

    added files

--- frontend/src/Routes/ProtectedRoute.tsx
+++ frontend/src/Routes/ProtectedRoute.tsx
@@ -10,7 +10,7 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const token = localStorage.getItem("token");
 
     if(!token) {
-        return <Navigate to="/" />
+        return <Navigate to="/" />  
     }
     return children;
 }
